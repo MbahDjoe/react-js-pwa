ca0e57c6422e924e50f520dd060a56b018768cf4
Irwan Wiyono <irwan.wiyono@gmail.com>
2023-07-05 00:57:33 +0700

    - fixed firebase config - removed wrong component folder

--- src/component/firebaseConfig.tsx
+++ src/component/firebaseConfig.tsx
@@ -3,20 +3,20 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import "firebase/firestore";
 const config = {
-    apiKey: "AIzaSyDdzT4nKugAkHrqXEMbkqNOz8CMUu2mf_c",
-    authDomain: "auth.technicalheist.com",
-    databaseURL: "https://my-first-project-eb9c5.firebaseio.com",
-    projectId: "my-first-project-eb9c5",
-    storageBucket: "my-first-project-eb9c5.appspot.com",
-    messagingSenderId: "171865625679",
-    appId: "1:171865625679:web:ed987997df37767f3ec63a"
-  };
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
 
-  if (!firebase.apps.length) {
+if (!firebase.apps.length) {
     firebase.initializeApp(config);
- }else {
+} else {
     firebase.app(); // if already initialized, use that one
- }
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
